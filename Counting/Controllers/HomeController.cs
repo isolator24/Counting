df47abdc807679d22f4f88bdbe1570980bc55969
Anthony <isolator24@gmail.com>
2016-06-29 18:39:51 +0800

    first commit

---
+++ Counting/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿
+using Counting.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Counting.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ExamEntities db = new ExamEntities();
+        
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(db.Incrementers.Single());
+        }
+
+        [HttpPost]
+        public ActionResult Index(int id)
+        {
+            Incrementer inc = db.Incrementers.Find(id);
+            if (inc.Count < 10)
+            {
+                inc.Count++;
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(inc).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            return View(db.Incrementers.Single());
+        }
+    }
+}
---
+++ Counting/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Counting
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            //Database.SetInitializer<Counting.Models.DataContext>(null);
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
